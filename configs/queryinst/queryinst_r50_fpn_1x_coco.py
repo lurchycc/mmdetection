696cdfa01d4c17ea6dc0e6f220be0e35bcfcbe33
lurchycc <204654446@qq.com>
2022-04-02 10:29:11 +0800

    1.2

--- configs/queryinst/queryinst_r50_fpn_1x_coco.py
+++ configs/queryinst/queryinst_r50_fpn_1x_coco.py
@@ -132,7 +132,34 @@ model = dict(
         ]),
     test_cfg=dict(
         rpn=None, rcnn=dict(max_per_img=num_proposals, mask_thr_binary=0.5)))
-
+#date argument
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1333, 800),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
 # optimizer
 optimizer = dict(
     _delete_=True,
